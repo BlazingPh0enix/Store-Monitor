0692eb7498111183c3f45c97cda234318d83c4f4
BlazingPh0enix <anaskazi6789@gmail.com>
2025-09-01 01:09:55 +0530

    Refactor database models to correct primary key definitions and update column types

--- app/models.py
+++ app/models.py
@@ -7,23 +7,24 @@ class Base(DeclarativeBase):
 class StoreStatus(Base):
     __tablename__ = "store_status"
 
-    store_id = Column(String, primary_key=True)
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    store_id = Column(String)
     status = Column(String, nullable=False)
-    timestamp_utc = Column(DateTime, nullable=False)
+    timestamp_utc = Column(String, nullable=False)
 
 class BusinessHours(Base):
     __tablename__ = "business_hours"
-
-    store_id = Column(String, primary_key=True)
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    store_id = Column(String)
     day_of_week = Column(Integer, nullable=False)
-    start_time_local = Column(DateTime, nullable=False)
-    end_time_local = Column(DateTime, nullable=False)
+    start_time_local = Column(String, nullable=False)
+    end_time_local = Column(String, nullable=False)
 
 class TimeZones(Base):
-    __tablename__ = "time_zones"
+    __tablename__ = "timezones"
 
     store_id = Column(String, primary_key=True)
-    time_zone = Column(String, nullable=False)
+    timezone_str = Column(String, nullable=False)
 
 class StoreReport(Base):
     __tablename__ = "store_report"
